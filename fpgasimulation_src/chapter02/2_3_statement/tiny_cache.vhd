c23f23c5501a355e353a42c6754093325dd2b48a
Alexander Schmid <alexander.schmid@mixed-mode.de>
2021-03-30 20:31:31 +0200

    Adapt chapter 2.3.

--- fpgasimulation_src/chapter02/2_3_statement/tiny_cache.vhd
+++ fpgasimulation_src/chapter02/2_3_statement/tiny_cache.vhd
@@ -12,21 +12,7 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
--- ********************************************************************-- *******************************************************************
--- Copyright 2008 Ray Salemi
-
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License. 
--- You may obtain a copy of the License at
---
---     http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
--- ********************************************************************--
+-- ********************************************************************
 -- VHDL Entity tinycache_sv_lib.tiny_cache_vhd.symbol
 --
 -- File   : tiny_cache_vhd_fsm.vhd
@@ -41,27 +27,27 @@
 -- Entity description for VHDL version of the Tinycache
 
 LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.std_logic_arith.all;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_arith.ALL;
 
-USE work.cache_vhd_pkg.all;
+USE work.cache_vhd_pkg.ALL;
 
 ENTITY tiny_cache_vhd IS
-   PORT( 
-      clk         : IN     std_logic;
-      cpu_address : IN     address_t;
-      cpu_rd      : IN     std_logic;
-      cpu_wr      : IN     std_logic;
-      rst         : IN     std_logic;
-      cpuwait     : OUT    std_logic;
-      mem_address : OUT    address_t;
-      mem_rd      : OUT    std_logic;
-      mem_wr      : OUT    std_logic;
-      cpu_data    : INOUT  data_t;
-      mem_data    : INOUT  data_t
+   PORT (
+      clk : IN STD_LOGIC;
+      cpu_address : IN address_t;
+      cpu_rd : IN STD_LOGIC;
+      cpu_wr : IN STD_LOGIC;
+      rst : IN STD_LOGIC;
+      cpuwait : OUT STD_LOGIC;
+      mem_address : OUT address_t;
+      mem_rd : OUT STD_LOGIC;
+      mem_wr : OUT STD_LOGIC;
+      cpu_data : INOUT data_t;
+      mem_data : INOUT data_t
    );
 
-END tiny_cache_vhd ;
+END tiny_cache_vhd;
 
 --
 -- VHDL Architecture tinycache_sv_lib.tiny_cache_vhd.fsm
@@ -78,20 +64,20 @@ END tiny_cache_vhd ;
 -- VHDL version of the Tinycache
 
 LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.std_logic_arith.all;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_arith.ALL;
 
-USE work.cache_vhd_pkg.all;
- 
+USE work.cache_vhd_pkg.ALL;
+
 ARCHITECTURE fsm OF tiny_cache_vhd IS
 
    -- Architecture Declarations
-   SIGNAL cache_address : std_logic_vector( 3 DOWNTO 0 );  
-   SIGNAL cache_ram     : cache_ram_t;  
-   SIGNAL key_ram       : key_ram_t;  
-   SIGNAL invalid       : std_logic_vector( cache_depth DOWNTO 0 );  
-   SIGNAL cpuwait_int   : std_logic;  
-   SIGNAL mem_wr_int    : std_logic;  
+   SIGNAL cache_address : STD_LOGIC_VECTOR(3 DOWNTO 0);
+   SIGNAL cache_ram : cache_ram_t;
+   SIGNAL key_ram : key_ram_t;
+   SIGNAL invalid : STD_LOGIC_VECTOR(cache_depth DOWNTO 0);
+   SIGNAL cpuwait_int : STD_LOGIC;
+   SIGNAL mem_wr_int : STD_LOGIC;
 
    TYPE STATE_TYPE IS (
       RESET,
@@ -99,7 +85,7 @@ ARCHITECTURE fsm OF tiny_cache_vhd IS
       WRITE,
       MISS
    );
- 
+
    -- Declare current and next state signals
    SIGNAL current_state : STATE_TYPE;
    SIGNAL next_state : STATE_TYPE;
@@ -107,10 +93,10 @@ ARCHITECTURE fsm OF tiny_cache_vhd IS
 BEGIN
 
    -----------------------------------------------------------------
-   clocked_proc : PROCESS ( 
+   clocked_proc : PROCESS (
       clk
-   )
-   -----------------------------------------------------------------
+      )
+      -----------------------------------------------------------------
    BEGIN
       IF (clk'EVENT AND clk = '1') THEN
          IF (rst = '1') THEN
@@ -120,15 +106,15 @@ BEGIN
 
             -- Combined Actions
             CASE current_state IS
-               WHEN RESET => 
-                  invalid <= (others => '1');
-               WHEN WRITE => 
-                  key_ram(conv_integer(unsigned(cache_address)))   <= cpu_address;
-                  invalid(conv_integer(unsigned(cache_address)))   <= '0';
+               WHEN RESET =>
+                  invalid <= (OTHERS => '1');
+               WHEN WRITE =>
+                  key_ram(conv_integer(unsigned(cache_address))) <= cpu_address;
+                  invalid(conv_integer(unsigned(cache_address))) <= '0';
                   cache_ram(conv_integer(unsigned(cache_address))) <= cpu_data;
-               WHEN MISS => 
-                  key_ram(conv_integer(unsigned(cache_address)))   <= cpu_address;
-                  invalid(conv_integer(unsigned(cache_address)))   <= '0';
+               WHEN MISS =>
+                  key_ram(conv_integer(unsigned(cache_address))) <= cpu_address;
+                  invalid(conv_integer(unsigned(cache_address))) <= '0';
                   cache_ram(conv_integer(unsigned(cache_address))) <= mem_data;
                WHEN OTHERS =>
                   NULL;
@@ -136,71 +122,75 @@ BEGIN
          END IF;
       END IF;
    END PROCESS clocked_proc;
- 
+
    -----------------------------------------------------------------
-   nextstate_proc : PROCESS ( 
+   nextstate_proc : PROCESS (
       cpu_wr,
       cpuwait_int,
       current_state
-   )
-   -----------------------------------------------------------------
+      )
+      -----------------------------------------------------------------
    BEGIN
       CASE current_state IS
-         WHEN RESET => 
+         WHEN RESET =>
             next_state <= HIT;
-         WHEN HIT => 
-            IF (cpuwait_int = '1') THEN 
+         WHEN HIT =>
+            IF (cpuwait_int = '1') THEN
                next_state <= MISS;
-            ELSIF (cpu_wr = '1') THEN 
+            ELSIF (cpu_wr = '1') THEN
                next_state <= WRITE;
             ELSE
                next_state <= HIT;
             END IF;
-         WHEN WRITE => 
+         WHEN WRITE =>
             next_state <= HIT;
-         WHEN MISS => 
+         WHEN MISS =>
             next_state <= HIT;
-  -- coverage off
+            -- coverage off
          WHEN OTHERS =>
             next_state <= RESET;
-  -- coverage on
+            -- coverage on
       END CASE;
    END PROCESS nextstate_proc;
- 
+
    -----------------------------------------------------------------
-   output_proc : PROCESS ( 
+   output_proc : PROCESS (
       current_state
-   )
-   -----------------------------------------------------------------
+      )
+      -----------------------------------------------------------------
    BEGIN
       -- Default Assignment
-      mem_rd <= '0'; mem_wr_int <= '0';
+      mem_rd <= '0';
+      mem_wr_int <= '0';
 
       -- Combined Actions
       CASE current_state IS
-         WHEN HIT => 
-            mem_wr_int 
-               <= '0';
-         WHEN WRITE => 
+         WHEN HIT =>
+            mem_wr_int
+            <= '0';
+         WHEN WRITE =>
             mem_wr_int <= '1';
-         WHEN MISS => 
+         WHEN MISS =>
             mem_rd <= '1';
          WHEN OTHERS =>
             NULL;
       END CASE;
    END PROCESS output_proc;
- 
+
    -- Concurrent Statements
-   cpu_data      <= cache_ram(conv_integer(unsigned(cache_address))) WHEN cpu_rd = '1' ELSE (others => 'Z');
-   mem_data      <= cpu_data WHEN mem_wr_int = '1' ELSE (others => 'Z');
-   cache_address <= cpu_address(3 downto 0);
-   mem_address   <= cpu_address;
-   cpuwait_int   <= '1' WHEN
-      (((key_ram(conv_integer(unsigned(cache_address))) /= cpu_address) 
-      OR (invalid(conv_integer(unsigned(cache_address))) = '1')) 
+   cpu_data <= cache_ram(conv_integer(unsigned(cache_address))) WHEN cpu_rd = '1' ELSE
+      (OTHERS => 'Z');
+   mem_data <= cpu_data WHEN mem_wr_int = '1' ELSE
+      (OTHERS => 'Z');
+   cache_address <= cpu_address(3 DOWNTO 0);
+   mem_address <= cpu_address;
+   cpuwait_int <= '1' WHEN
+      (((key_ram(conv_integer(unsigned(cache_address))) /= cpu_address)
+      OR (invalid(conv_integer(unsigned(cache_address))) = '1'))
       AND (cpu_rd = '1'))
-   ELSE '0';
-   cpuwait       <= cpuwait_int;
-   mem_wr        <= mem_wr_int;
-   
+      ELSE
+      '0';
+   cpuwait <= cpuwait_int;
+   mem_wr <= mem_wr_int;
+
 END fsm;
